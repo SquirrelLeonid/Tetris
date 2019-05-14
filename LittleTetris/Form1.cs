f47f6abac17c2f69d9a26740e407264e36df2c31
sqirelLeonid <sqirelLeonid@gmail.com>
2019-05-15 03:41:51 +0500

    Преписал методы
    Дописал метод движения фигуры ВНИЗ, метод для проверки возможности падения. Многое перенесено в статический класс GameModel. В этом коммите полностью работает падение фигур вниз

--- LittleTetris/Form1.cs
+++ LittleTetris/Form1.cs
@@ -9,145 +9,66 @@ namespace LittleTetris
     
     public partial class TetrisForm : Form
     {
-        #region Объявление полей, задание размеров и прочее
-        public const int width = 15, height = 25, cellSize = 15;
         private int currentIteration = 0;
-        public int[,] figure = new int[2, 4];   
-        public int[,] field = new int[width, height];
-        public Bitmap background = new Bitmap(cellSize * (width + 1), cellSize * (height + 1));
-        public readonly GameModel gameModel = new GameModel();
-        #endregion
+        public Bitmap background;
 
-        public static bool[,] field2 = new bool[width, height]; //Изначально заполненно false-ми, static для доступа извне
         public TetrisForm()
         {
-            InitializeComponent();            
-            figure = gameModel.CreateFigure();
-        } //Самая первая фигура создается здесь
+            InitializeComponent();
+            background = new Bitmap(GameModel.cellSize * (GameModel.width + 1), GameModel.cellSize * (GameModel.height + 1));
+        }
 
-        //Пусть остается как есть
+        private void TickTimer_Tick(object sender, EventArgs e)
+        {            
+            GameModel.figure.MoveDown(); // Вызываем метод, в результате которого фигура опускается
+            GameModel.lineChecker.FindFilledLines(); //Находим и уничтожаем заполненные линии если такие есть
+            FillField(); //После падения и уничтожения заполненных линий перерисовываем
+            IterationCounter.Text = currentIteration++.ToString();
+            GameModel.lineChecker.IsTooHigh();
+        }
+
         public void FillField()
         {
             Graphics graphics = Graphics.FromImage(background);
             graphics.Clear(Color.Black);
-            graphics.DrawRectangle(Pens.Red, cellSize - 1, cellSize, (width - 1) * cellSize, (height - 1) * cellSize);
+            graphics.DrawRectangle(
+                Pens.Red,
+                GameModel.cellSize - 1,
+                GameModel.cellSize,
+                (GameModel.width - 1) * GameModel.cellSize,
+                (GameModel.height - 1) * GameModel.cellSize);
             //Покраска приземлившихся фигур
-            for (int i = 0; i < width; i++)
-                for (int j = 0; j < height; j++)
-                    if (field[i,j] == 1) // избавляет от проблемы с закрашиванием всего поля
-                    graphics.FillRectangle(
-                        Brushes.Green,
-                        i * cellSize,
-                        j * cellSize,
-                        (cellSize - 1) * 1, //Для зазоров
-                        (cellSize - 1) * 1); // пофакту, оно красит все поле, но там где
-            //клеток нет, получаются зеленые вырожденные квадраты. Если заменить на просто 1, то закрасится все
-
+            for (int i = 0; i < GameModel.width; i++)
+                for (int j = 0; j < GameModel.height; j++)
+                    if (GameModel.field[i, j] == true) // избавляет от проблемы с закрашиванием всего поля
+                        graphics.FillRectangle(
+                            Brushes.Green,
+                            i * GameModel.cellSize,
+                            j * GameModel.cellSize,
+                            GameModel.cellSize - 1, //Для зазоров
+                            GameModel.cellSize - 1);
             //Покраска падающих фигур
             for (int i = 0; i < 4; i++)
+            {
+                Point cell = GameModel.figure.CellsCoordinates[i];
                 graphics.FillRectangle(
                     Brushes.Red,
-                    figure[1, i] * cellSize,
-                    figure[0, i] * cellSize,
-                    cellSize - 1, //Для зазоров
-                    cellSize - 1); //Для зазоров
+                    cell.X * GameModel.cellSize,
+                    cell.Y * GameModel.cellSize,
+                    GameModel.cellSize - 1, //Для зазоров
+                    GameModel.cellSize - 1); //Для зазоров
+            }
 
             FieldPictureBox.Image = background; // Обновление состояния окна после отрисовки фигур
         }
-
-        private void TickTimer_Tick(object sender, EventArgs e)
-        {
-            if (field[8, 4] == 1)
-                Environment.Exit(0);
-            DestroyFilledLines();
-            Move(0, 1);
-            IterationCounter.Text = currentIteration++.ToString();
-            
-        }
-
+      
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            //Тут у меня че то с табуляцией 
             switch (e.KeyCode)
             {
-                case Keys.A: Move(-1, 0); break;
-                case Keys.D: Move(1, 0); break;
-                case Keys.S: TickTimer.Interval = 50; break;
-                
-                //Опиши как у тебя действует поворот
-                case Keys.W:               
-                int[,] shapeT = new int[2, 4];
-                //Копируем текущую фигуру
-                Array.Copy(figure, shapeT, figure.Length);
-                //Тут ты определяешь положение фигуры??
-                int maxX = Enumerable.Range(0, 4).Select(j => figure[1, j]).ToArray().Max();
-                int maxY = Enumerable.Range(0, 4).Select(j => figure[0, j]).ToArray().Max();
-
-                for (int i = 0; i < 4; i++)
-                { 
-                    int temp = figure[0, i];
-                    figure[0, i] = maxY - (maxX - figure[1, i]) - 1;
-                    figure[1, i] = maxX - (3 - (maxY - temp)) + 1;
-                }
-                //Если после поворота залезли куда не надо
-                if (FindMistake())
-                    Array.Copy(shapeT, figure, figure.Length);
-                break;
+                case Keys.S: TickTimer.Interval = 50; break;               
             }
         }
-        private void Form1_KeyUp(object sender, KeyEventArgs e) => TickTimer.Interval = 250;
-   
-
-        public void DestroyFilledLines()
-        {
-            IEnumerable<int> lines = Enumerable.Range(0, height);
-            IEnumerable<int> columns = Enumerable.Range(0, width);
-            //Проходит по всему полю и проверяет заполненность каждой строки, если она заполнена
-            //То добавляется в Enumerable и затем строки с этими индексами смещаются
-            IEnumerable<int> linqCall = from i in lines
-                                        where columns.Select(j => field[j, i]).Sum() >= width - 1
-                                        select i;
-            foreach (int i in linqCall)
-            {
-                for (int k = i; k > 1; k--)
-                    for (int l = 1; l < width; l++)
-                        field[l, k] = field[l, k - 1];
-            }
-        }
-        //Возможно вынести в модель?
-        public new void Move(int xSpeed, int ySpeed)
-        {        
-            for (int i = 0; i < 4; i++)
-            {
-                figure[1, i] += xSpeed;
-                figure[0, i] += ySpeed;
-            }
-
-            if (FindMistake())
-            { 
-                Move(-xSpeed, -ySpeed);
-                //Что делает эта часть?
-                if (ySpeed != 0)
-                {
-                    for (int i = 0; i < 4; i++)
-                        field[figure[1, i], figure[0, i]]++;
-                    figure = gameModel.CreateFigure(); 
-                }
-            }
-            FillField();
-        }
-
-        public bool FindMistake()
-        {
-            for (int i = 0; i < 4; i++)
-                //Первые четыре строки - выход за границы, последняя - наложение
-                if (figure[1, i] == width
-                    || figure[0, i] == height
-                    || figure[1, i] == 0
-                    || figure[0, i] == 0
-                    || field[figure[1, i], figure[0, i]] == 1)
-                    return true;          
-            return false;
-        }
+        private void Form1_KeyUp(object sender, KeyEventArgs e) => TickTimer.Interval = 250;             
     }
 }
--- LittleTetris/GameModel.cs
+++ LittleTetris/GameModel.cs
@@ -1,119 +1,198 @@
 ﻿using System;
 using System.Drawing;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LittleTetris
 {
     
-    public class GameModel
+    public static class GameModel
     {
-        public readonly int ScoreCount = 10;
-        //Этот метод потом уберется. Пока что он нужен для того чтобы код запускался
-        public int[,] CreateFigure()
-        {
-            Random x = new Random();
-            int[,] figure = null;
-            switch (x.Next(7)) // Рандомно выбираем 1 из 7 возможных фигур
-            {
-                //Первые скобки - Y, Вторые - Х
-                case 0: figure = new int[,] { { 2, 3, 4, 5 }, { 7, 7, 7, 7 } }; break; //I
-                case 1: figure = new int[,] { { 2, 3, 2, 3 }, { 7, 7, 8, 8 } }; break; //O
-                case 2: figure = new int[,] { { 2, 3, 4, 4 }, { 7, 7, 7, 8 } }; break; //J
-                case 3: figure = new int[,] { { 2, 3, 4, 4 }, { 7, 7, 7, 6 } }; break; //L
-                case 4: figure = new int[,] { { 3, 3, 4, 4 }, { 6, 7, 7, 8 } }; break; //Z
-                case 5: figure = new int[,] { { 3, 3, 4, 4 }, { 8, 7, 7, 6 } }; break; //S
-                case 6: figure = new int[,] { { 3, 4, 4, 4 }, { 7, 6, 7, 8 } }; break; //Т
-            }
-            return figure;
-        }
-    }
+        //public readonly int ScoreCount = 10;       
+        public const int width = 15, height = 25, cellSize = 15;
+        public static readonly bool[,] field = new bool[width, height];
+        public static Figure figure = new Figure();
+        public static LineChecker lineChecker = new LineChecker();
 
-    public class Figure
-    {
-        private enum Figures
+        public class Figure
         {
-            O = 0, //Квадрат
-            I = 1, //Палка
-            J = 2, //Углы
-            L = 3,
-            Z = 4, //Зиг-заги
-            S = 5,
-            T = 6 // Т-образная
-        }
-        public bool IsFallen;
-        public List<Point> CellsCoordinates;
+            private enum Figures
+            {
+                O = 0, //Квадрат
+                I = 1, //Палка
+                J = 2, //Углы
+                L = 3,
+                Z = 4, //Зиг-заги
+                S = 5,
+                T = 6 // Т-образная
+            }
+            public bool IsFallen;
+            public List<Point> CellsCoordinates;
 
-        public Figure()
-        {
-            CellsCoordinates = new List<Point>(4);
-            Figures type = (Figures)new Random().Next(7);
-            #region Блок If-ов
-            if (type == Figures.O)
+            public Figure()
             {
-                CellsCoordinates.Add(new Point(7,3));
-                CellsCoordinates.Add(new Point(7,3));
-                CellsCoordinates.Add(new Point(8,2));
-                CellsCoordinates.Add(new Point(8,3));              
-            }             
-            if (type == Figures.I)
-            {
-                CellsCoordinates.Add(new Point(7, 2));
-                CellsCoordinates.Add(new Point(7, 3));
-                CellsCoordinates.Add(new Point(7, 4));
-                CellsCoordinates.Add(new Point(7, 5));
+                IsFallen = false;
+                CellsCoordinates = new List<Point>(4);
+                Figures type = (Figures)new Random().Next(7);
+                #region Блок If-ов
+                if (type == Figures.O)
+                {
+                    CellsCoordinates.Add(new Point(7, 2));
+                    CellsCoordinates.Add(new Point(7, 3));
+                    CellsCoordinates.Add(new Point(8, 2));
+                    CellsCoordinates.Add(new Point(8, 3));
+                }
+                if (type == Figures.I)
+                {
+                    CellsCoordinates.Add(new Point(7, 2));
+                    CellsCoordinates.Add(new Point(7, 3));
+                    CellsCoordinates.Add(new Point(7, 4));
+                    CellsCoordinates.Add(new Point(7, 5));
+                }
+                if (type == Figures.J)
+                {
+                    CellsCoordinates.Add(new Point(7, 2));
+                    CellsCoordinates.Add(new Point(7, 3));
+                    CellsCoordinates.Add(new Point(7, 4));
+                    CellsCoordinates.Add(new Point(6, 4));
+                }
+                if (type == Figures.L)
+                {
+                    CellsCoordinates.Add(new Point(7, 2));
+                    CellsCoordinates.Add(new Point(7, 3));
+                    CellsCoordinates.Add(new Point(7, 4));
+                    CellsCoordinates.Add(new Point(8, 4));
+                }
+                if (type == Figures.Z)
+                {
+                    CellsCoordinates.Add(new Point(6, 3));
+                    CellsCoordinates.Add(new Point(7, 3));
+                    CellsCoordinates.Add(new Point(7, 4));
+                    CellsCoordinates.Add(new Point(8, 4));
+                }
+                if (type == Figures.S)
+                {
+                    CellsCoordinates.Add(new Point(8, 3));
+                    CellsCoordinates.Add(new Point(7, 3));
+                    CellsCoordinates.Add(new Point(7, 4));
+                    CellsCoordinates.Add(new Point(6, 4));
+                }
+                if (type == Figures.T)
+                {
+                    CellsCoordinates.Add(new Point(7, 3));
+                    CellsCoordinates.Add(new Point(6, 4));
+                    CellsCoordinates.Add(new Point(7, 4));
+                    CellsCoordinates.Add(new Point(8, 4));
+                }
+                #endregion
             }
-            if (type == Figures.J)
-            {
-                CellsCoordinates.Add(new Point(7, 2));
-                CellsCoordinates.Add(new Point(7, 3));
-                CellsCoordinates.Add(new Point(7, 4));
-                CellsCoordinates.Add(new Point(8, 4));
-            }
-            if (type == Figures.L)
-            {
-                CellsCoordinates.Add(new Point(7, 2));
-                CellsCoordinates.Add(new Point(7, 3));
-                CellsCoordinates.Add(new Point(7, 4));
-                CellsCoordinates.Add(new Point(6, 4));
-            }
-            if (type == Figures.Z)
-            {
-                CellsCoordinates.Add(new Point(6, 3));
-                CellsCoordinates.Add(new Point(7, 3));
-                CellsCoordinates.Add(new Point(7, 4));
-                CellsCoordinates.Add(new Point(8, 4));
-            }
-            if (type == Figures.S)
-            {
-                CellsCoordinates.Add(new Point(8, 3));
-                CellsCoordinates.Add(new Point(7, 3));
-                CellsCoordinates.Add(new Point(7, 4));
-                CellsCoordinates.Add(new Point(6, 4));
-            }
-            if (type == Figures.T)
-            {
-                CellsCoordinates.Add(new Point(7, 3));
-                CellsCoordinates.Add(new Point(6, 4));
-                CellsCoordinates.Add(new Point(7, 4));
-                CellsCoordinates.Add(new Point(8, 4 ));
-            }
-            #endregion
-        }
 
-        //Здесь не будет проверки на столкновение с другой фигурой
-        //Метод выполняет только 2 функции: двигает фигуру вниз если она не упала.
-        //Проверка на то, что фигура достигла дна будет в другом месте. Если условие этой проверки
-        //Соблюдается (Фигура достигла дна), то полю IsFallen присваивается True
-        public void MoveDown()
-        {
-            if (!IsFallen) //Если фигура не упала
-            {               
+            //Обновляет состояние поля после создания новой фигуры.
+            //Вызывается единожды для каждой новой фигуры
+
+            //**
+            private void UpdateFieldState()
+            {
                 for (int i = 0; i < 4; i++) //Все фигуры из 4 клеток
                 {
                     Point cell = CellsCoordinates[i];
-                    CellsCoordinates[i] = new Point(cell.X, cell.Y + 1);
+                    field[cell.X, cell.Y] = true; // Там где стоит фигура после появления ставим true
+                    figure = new Figure();
+                }
+            }
+
+            //РАБОТАЕТ
+            public void MoveDown()
+            {
+                if (!IsFallen) //Если фигура еще не упала
+                {
+                    //Сначала проверяем возможность падения
+                    if (!CheckDown())
+                    {
+                        IsFallen = true;
+                        UpdateFieldState();
+                    }
+                    //Если фигура не упала и есть возможность двигаться вниз
+                    else
+                    {
+                        Point cell;
+                        for (int i = 0; i < 4; i++) //Все фигуры из 4 клеток
+                        {
+                            cell = CellsCoordinates[i];
+                            CellsCoordinates[i] = new Point(cell.X, cell.Y + 1);
+                        }
+                    }
+                }
+            }
+
+            //РАБОТАЕТ
+            //Проверяем пространство под фигурой
+            private bool CheckDown() //Сложность N
+            {
+                Point cell;
+                for (int i = 0; i < 4; i++) //Все фигуры из 4 клеток
+                {
+                    cell = CellsCoordinates[i];
+                    #region Пояснения к проверке
+                    //Если клетка под фигурой уже занята или фигура достигла дна
+                    //Условие должно быть верным, потому что сначала идет проверка на корректность,
+                    //А уже потом происходит падение. Т.Е ситуации, когда фигура упала за поле, или залезла в другую
+                    //Фигуру быть не может.
+                    //Кроме того, мне не нужно проверять принадлежит ли новая координата одной из клеток фигуры,
+                    //Потому что нет необходимость обозначать на поле true/false места, где находится
+                    //ПАДАЮЩАЯ фигура
+                    #endregion
+                    if (CellsCoordinates[i].Y == height - 1
+                        || field[cell.X, cell.Y + 1] == true)
+                        return false; //фигура упала
+                }
+                return true;
+            }
+        }
+
+        public class LineChecker
+        {
+            //Сложность N^2
+            public void IsTooHigh()
+            {
+                for (int i = 0; i < width; i++)
+                {
+                    if (field[i, 3])
+                        Environment.Exit(0);
+                }
+            }
+            public IEnumerable<int> FindFilledLines()
+            {
+                bool lineIsFilled;
+                for (int i = 5; i < height; i++)
+                {
+                    lineIsFilled = true;
+                    for (int j = 0; j < width; j++)
+                    {
+                        if (!field[i, j]) // Если встречается хоть одна пустая клетка
+                        {
+                            lineIsFilled = false; // Линия не заполнена
+                            break;
+                        }
+                    }
+                    if (lineIsFilled) //Если линия все таки заполнена
+                        yield return i; //Возвращаем индекс строки
+                }
+            }
+
+            //Сложность N^2
+            private void DestroyFilledLines(IEnumerable<int> filledLines) 
+            {
+                if (filledLines.Count() == 0)
+                    return;
+                filledLines.Reverse(); //Устанавливаем порядок по убыванию (т.е. начинаем с низа)
+                foreach (int line in filledLines)
+                {
+                    for (int i = 0; i < width; i++)
+                        field[line, i] = field[line - 1, i]; //Нижнюю строку заменяем верхней поэлементно.
                 }
             }
         }
+        
     }
 }
